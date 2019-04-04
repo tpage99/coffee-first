58501a9c16ae144c95599a9c52039337d8457ac7
tpage99 <taylorvtx1300@gmail.com>
2019-04-04 07:50:56 -0500

    changed some config data for coffee first title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Coffee First`,
+    description: `Putting coffee at the first part of your day, so you can take on the the rest of the world.`,
+    author: `@coffeFirst`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
